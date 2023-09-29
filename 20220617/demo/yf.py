76b5b404c8da43c3a6392116ee64b7836b99bc84
Paul T <4277512+pault-tech@users.noreply.github.com>
2023-09-29 07:56:15 -1000

    pd

--- 20220617/demo/yf.py
+++ 20220617/demo/yf.py
@@ -1,4 +1,14 @@
 import yfinance as yf
+import pandas as pd
+
+# Define the ticker symbol
+ticker = "NVDA"
+
+# Download the stock data at the second granularity
+data = yf.download(ticker, interval="1s")
+
+# Convert the data to a Parquet dataset
+data.to_parquet("nvda_stock_data.parquet")
 
 msft = yf.Ticker("MSFT")
 
